b79022eda9d5fd437035c5671f9cc22c13c33192
Luis Garcia <luisgarcia26284@gmail.com>
2024-07-02 11:10:59 +0800

    (NVim) Adding: Outline

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -8,6 +8,17 @@ return {
 		},
 	},
 	{
+		"hedyhli/outline.nvim",
+		config = function()
+			-- Example mapping to toggle outline
+			vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Outline: Toggle" })
+
+			require("outline").setup({
+				-- Your setup opts here (leave empty to use defaults)
+			})
+		end,
+	},
+	{
 		"rcarriga/nvim-notify",
 		config = function()
 			local notify = require("notify")
